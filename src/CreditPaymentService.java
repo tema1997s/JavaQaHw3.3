dcdc6085c038a1afb639a0d1f2ba3fee4ebe3a58
Artem <tema1997s@gmail.com>
2023-04-21 14:52:50 +0300

    CreditPaymentService changed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,9 +3,9 @@ public class CreditPaymentService {
         int termMonth = termYear * 12;
         double monthPercent = yearPercent / 12 / 100;
         double ratio = (monthPercent * Math.pow(1 + monthPercent, termMonth)) / (Math.pow(1 + monthPercent, termMonth) - 1);
-        double result1 = sum * ratio;
-        int result = (int) result1;
-        return result;
+        double result = sum * ratio;
+        int monthlyPayment = (int) result;
+        return monthlyPayment;
 
     }
 }
